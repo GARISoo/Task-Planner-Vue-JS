ac5425b5daad51034cf9b0c40e607eb5d938bbb8
Haralds <haralds_treilons@inbox.lv>
2022-06-02 17:33:04 +0300

    Base done

---
+++ src/components/TaskApp.vue
@@ -0,0 +1,87 @@
+<style>
+@import "./TaskApp.scss";
+</style>
+
+<template>
+  <div class="task-planner">
+    <div class="task-add">
+      <input
+        class="input"
+        type="text"
+        placeholder="Your task"
+        v-model="inputValue"
+      />
+      <button @click="addTask()">Add</button>
+    </div>
+    <div class="single-task" v-for="task in visibleTasks" :key="task">
+      <span
+        class="task-text"
+        :class="{ completed: task.isDone }"
+        @click="toggleComplete(task.id)"
+      >
+        {{ task.title }}
+      </span>
+      <button class="delete-btn" @click="removeTask(task.id)">X</button>
+    </div>
+    <div class="task-filters">
+      <button @click="setFilter('All')">All</button>
+      <button @click="setFilter('In progress')">In progress</button>
+      <button @click="setFilter('Completed')">Completed</button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+type Task = {
+  id: number;
+  title: string;
+  isDone: boolean;
+};
+
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "Task-planner",
+  data: () => ({
+    inputValue: "",
+    tasks: [] as Task[],
+    id: 0,
+    filter: "All",
+  }),
+  methods: {
+    addTask() {
+      this.id++;
+      this.tasks = [
+        ...this.tasks,
+        {
+          id: this.id,
+          title: this.inputValue,
+          isDone: false,
+        },
+      ];
+      this.inputValue = "";
+    },
+    removeTask(id: number) {
+      this.tasks = this.tasks.filter((task) => task.id !== id);
+    },
+    toggleComplete(id: number) {
+      this.tasks = this.tasks.map((task) =>
+        task.id === id ? { ...task, isDone: !task.isDone } : { ...task }
+      );
+    },
+    setFilter(stage: string) {
+      this.filter = stage;
+    },
+  },
+  computed: {
+    visibleTasks() {
+      let filteredTasks;
+      if (this.filter === "All") filteredTasks = this.tasks;
+      if (this.filter === "In progress")
+        filteredTasks = this.tasks.filter((task) => !task.isDone);
+      if (this.filter === "Completed")
+        filteredTasks = this.tasks.filter((task) => task.isDone);
+      return filteredTasks;
+    },
+  },
+});
+</script>
